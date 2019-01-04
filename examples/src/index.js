0c9aa9aae0416d1490b524b8e9afa9a413b796cf
Adrian Henry <adrian@hungryturtlecode.com>
2019-01-04 18:14:42 +0000

    example: Use the offset passed in to create some scaling of elements

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,9 +1,20 @@
 import ParallaxProvider from '../../src';
 
-const {log} = window.console;
+const fg = document.querySelector('.foreground');
+const mg = document.querySelector('.middleground');
+const bg = document.querySelector('.background');
+const title = document.querySelector('.firstpage h1');
 
 function phaseOne(offset) {
-  log('phase one', offset);
+  const duration = 500;
+
+  if (offset < duration) {
+    const scaleAmt = 0.4 * Math.min(offset / duration, 1);
+    fg.style.transform = `scale(${1 + scaleAmt})`;
+    mg.style.transform = `scale(${1 + scaleAmt * 0.24})`;
+    bg.style.transform = `scale(${1 + scaleAmt * 0.1})`;
+    title.style.transform = `scale(${1 + scaleAmt * 0.8})`;
+  }
 }
 
 export default new ParallaxProvider([
