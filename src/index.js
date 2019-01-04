495f2040a288ecad6561b95522aa39b5104d0e11
Adrian Henry <adrian@hungryturtlecode.com>
2019-01-04 18:37:53 +0000

    feat: Update the mounting logic to mount modules relative to the end of the previous module

--- src/index.js
+++ src/index.js
@@ -7,11 +7,35 @@ export default class ParallaxProvider {
   }
 
   init() {
+    const newModules = [];
+
+    for (let i = 0; i < this.modules.length; i++) {
+      const module = this.modules[i];
+
+      const numNewModules = newModules.length;
+      const previousModule = numNewModules
+        ? newModules[numNewModules - 1]
+        : null;
+
+      let endPrevModule = 0;
+      if (previousModule) {
+        const prevDuration = previousModule.duration;
+        endPrevModule = previousModule._absMountPoint + prevDuration;
+      }
+
+      const absMountPoint = endPrevModule + module.mountPoint;
+      module._absMountPoint = absMountPoint;
+
+      newModules.push(module);
+    }
+
+    this.modules = newModules;
+
     window.addEventListener('scroll', () => {
       const yoff = window.pageYOffset;
 
       this.modules.forEach(module => {
-        module.controller(yoff - module.mountPoint, module.duration);
+        module.controller(yoff - module._absMountPoint, module.duration);
       });
     });
   }
