87ceba75a5dfb0e8589750ff89beaeee53460371
Adrian Henry <adrian@hungryturtlecode.com>
2019-01-08 19:56:53 +0000

    tests: Add more tests to cover different features / use cases

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,5 +1,13 @@
 import ParallaxProvider from '../src';
 
+let events = {};
+beforeEach(() => {
+  events = {};
+  global.addEventListener = jest.fn((event, cb) => {
+    events[event] = cb;
+  });
+});
+
 test('it correctly adds one module to modules array', () => {
   const p = new ParallaxProvider([{}]);
   expect(p.modules.length).toBe(1);
@@ -22,18 +30,12 @@ test('calls window.addEventListener() for scroll event', () => {
 });
 
 describe('scroll events with multiple sections', () => {
-  let events = {};
   let sectionOneCtrl = jest.fn();
   let sectionTwoCtrl = jest.fn();
   let sectionOneDur = 500;
   let sectionTwoDur = 800;
 
   beforeEach(() => {
-    events = {};
-    global.addEventListener = jest.fn((event, cb) => {
-      events[event] = cb;
-    });
-
     const p = new ParallaxProvider([
       {
         mountPoint: 0,
@@ -109,3 +111,242 @@ describe('scroll events with multiple sections', () => {
     );
   });
 });
+
+test('it to call the duration function', () => {
+  let durationFn = jest.fn();
+
+  const p = new ParallaxProvider([
+    {
+      mountPoint: 0,
+      duration: durationFn,
+      controller: () => {},
+    },
+  ]);
+
+  expect(durationFn).toBeCalled();
+});
+
+test('it to use the value from the duration function', () => {
+  let duration = 430;
+  let durationFn = jest.fn(() => duration);
+  let sectionCtrl = jest.fn();
+
+  const p = new ParallaxProvider([
+    {
+      mountPoint: 0,
+      duration: durationFn,
+      controller: sectionCtrl,
+    },
+  ]);
+  global.pageYOffset = 0;
+
+  events.scroll();
+
+  expect(sectionCtrl).toBeCalledWith(global.pageYOffset, duration);
+});
+
+test('it should handle negative mount point', () => {
+  let sectionCtrl = jest.fn();
+  let sectionDur = 800;
+  let mountPoint = -400;
+
+  const p = new ParallaxProvider([
+    {
+      mountPoint: 0,
+      duration: 500,
+      controller: () => {},
+    },
+    {
+      mountPoint: 0,
+      duration: 800,
+      controller: () => {},
+    },
+    {
+      mountPoint: mountPoint,
+      duration: sectionDur,
+      controller: sectionCtrl,
+    },
+  ]);
+
+  global.pageYOffset = 850;
+
+  events.scroll();
+
+  expect(sectionCtrl).toBeCalledWith(
+    global.pageYOffset - 1300 - mountPoint,
+    sectionDur,
+  );
+});
+
+test('it should handle absolute mounting', () => {
+  let sectionCtrl = jest.fn();
+  let sectionDur = 800;
+  let mountPoint = 400;
+
+  const p = new ParallaxProvider([
+    {
+      mountPoint: 0,
+      duration: 500,
+      controller: () => {},
+    },
+    {
+      mountPoint: 0,
+      duration: 800,
+      controller: () => {},
+    },
+    {
+      mountPoint: mountPoint,
+      mountType: 'absolute',
+      duration: sectionDur,
+      controller: sectionCtrl,
+    },
+  ]);
+
+  global.pageYOffset = 950;
+
+  events.scroll();
+
+  expect(sectionCtrl).toBeCalledWith(
+    global.pageYOffset - mountPoint,
+    sectionDur,
+  );
+});
+
+test('it should provide correct args when using mountAfterId', () => {
+  let sectionOneDur = 400;
+  let sectionThreeCtrl = jest.fn();
+  let sectionThreeDur = 800;
+  let sectionThreeMountPoint = -100;
+
+  const p = new ParallaxProvider([
+    {
+      id: 1,
+      mountPoint: 0,
+      duration: sectionOneDur,
+      controller: () => {},
+    },
+    {
+      mountPoint: 0,
+      duration: 800,
+      controller: () => {},
+    },
+    {
+      mountPoint: sectionThreeMountPoint,
+      mountAfterId: 1,
+      duration: sectionThreeDur,
+      controller: sectionThreeCtrl,
+    },
+  ]);
+
+  global.pageYOffset = 50;
+  events.scroll();
+
+  expect(sectionThreeCtrl).toBeCalledWith(
+    global.pageYOffset - sectionOneDur - sectionThreeMountPoint,
+    sectionThreeDur,
+  );
+});
+
+describe('Error states', () => {
+  test('throw error when mounting after a non existent id', () => {
+    expect(() => {
+      const p = new ParallaxProvider([
+        {
+          id: 1,
+          mountPoint: 0,
+          duration: 500,
+          controller: () => {},
+        },
+        {
+          mountPoint: 0,
+          duration: 800,
+          controller: () => {},
+        },
+        {
+          mountPoint: 0,
+          mountAfterId: 100,
+          duration: 500,
+          controller: () => {},
+        },
+      ]);
+    }).toThrow(Error);
+  });
+
+  test('throw error when mounting after itself', () => {
+    expect(() => {
+      const p = new ParallaxProvider([
+        {
+          mountPoint: 0,
+          duration: 500,
+          controller: () => {},
+        },
+        {
+          mountPoint: 0,
+          duration: 800,
+          controller: () => {},
+        },
+        {
+          id: 'test',
+          mountPoint: 0,
+          mountAfterId: 'test',
+          duration: 500,
+          controller: () => {},
+        },
+      ]);
+    }).toThrow(Error);
+  });
+
+  test('throw error when first module uses mountAfterId', () => {
+    expect(() => {
+      const p = new ParallaxProvider([
+        {
+          mountPoint: 0,
+          duration: 500,
+          mountAfterId: 'test',
+          controller: () => {},
+        },
+        {
+          id: 'test',
+          mountPoint: 0,
+          duration: 800,
+          controller: () => {},
+        },
+        {
+          mountPoint: 0,
+          duration: 500,
+          controller: () => {},
+        },
+      ]);
+    }).toThrow(Error);
+  });
+
+  test('throw error when two modules share an id', () => {
+    expect(() => {
+      const p = new ParallaxProvider([
+        {
+          id: 1,
+          mountPoint: 0,
+          duration: 500,
+          controller: () => {},
+        },
+        {
+          id: 1,
+          mountPoint: 0,
+          duration: 800,
+          controller: () => {},
+        },
+        {
+          mountPoint: 0,
+          duration: 500,
+          controller: () => {},
+        },
+      ]);
+    }).toThrow(Error);
+  });
+});
+
+// TODO Tue 08 Jan 2019 19:23:42 GMT
+// use different types as id to see what happens
+// missing properties in module obj (ie provide defaults)
+//
+// provide warning when using mountAfterId and absolute mount type together
